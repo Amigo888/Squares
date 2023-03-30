de3ce2e08153ba7df42d99bedad3ac703b367623
Dima Protsak <protsack.dima25@gmail.com>
2023-03-30 13:17:03 +0300

    VC

---
+++ Squares/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Squares
+//
+//  Created by Дмитрий Процак on 30.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
