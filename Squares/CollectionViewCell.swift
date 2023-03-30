0234b0bfbb7a22c9b543128e68719f966dd9846b
Dima Protsak <protsack.dima25@gmail.com>
2023-03-30 13:49:37 +0300

    setup collectionView and constraints

---
+++ Squares/CollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewCell.swift
+//  Squares
+//
+//  Created by Дмитрий Процак on 30.03.2023.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    static let identifier = "CollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Squares/ViewController.swift
+++ Squares/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 110, height: 400)
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.identifier)
+        layout.minimumLineSpacing = 20
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+       collectionView.frame = view.bounds
+    }
 }
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let item = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.identifier, for: indexPath)
+        item.backgroundColor = .blue
+        
+        return item
+    }
+    
+    
+}
+
 
